6b1000387775ff4f24bea29e3083cb7d8e2d0daf
shivam rathi <shivamrathi011@gmail.com>
2026-05-20 14:20:50 +0530

    Update working directory for Terraform commands and comment out S3 bucket resources

--- terraform/dev/backend-s3.tf
+++ terraform/dev/backend-s3.tf
@@ -1,17 +1,17 @@
-resource "aws_s3_bucket" "survey-s3" {
-  bucket = "survey-terraform-state"
+# resource "aws_s3_bucket" "survey-s3" {
+#   bucket = "survey-terraform-state"
 
-  tags = {
-    Name        = "survey-terraform-state"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "survey-terraform-state"
+#     Environment = "Dev"
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "survey-s3-public-access" {
-  bucket = aws_s3_bucket.survey-s3.id
+# resource "aws_s3_bucket_public_access_block" "survey-s3-public-access" {
+#   bucket = aws_s3_bucket.survey-s3.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
