285dc726cbc1508e557055417aaae2205a4a7a98
shivam rathi <shivamrathi011@gmail.com>
2026-05-21 16:36:41 +0530

    fix

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -172,18 +172,45 @@ dex:
 YAML
   ]
 
-  argocd_app_enabled               = true
-  argocd_app_name                  = "${var.project}-${var.env}-app"
-  argocd_app_project               = var.argocd_app_project
-  argocd_app_repo_url              = var.argocd_app_repo_url
-  argocd_app_repo_path             = var.argocd_app_repo_path
-  argocd_app_repo_revision         = var.argocd_app_repo_revision
-  argocd_app_destination_namespace = var.argocd_app_destination_namespace
-  argocd_app_sync_enabled          = var.argocd_app_sync_enabled
-  argocd_app_prune                 = var.argocd_app_prune
-  argocd_app_self_heal             = var.argocd_app_self_heal
-
   depends_on = [module.eks_addons]
+}
+
+# Create ArgoCD Application resource to monitor the Git repository
+resource "kubernetes_manifest" "argocd_application" {
+  count = var.argocd_app_repo_url != "" ? 1 : 0
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "${var.project}-${var.env}-app"
+      namespace = "argocd"
+    }
+    spec = {
+      project = var.argocd_app_project
+      source = {
+        repoURL        = var.argocd_app_repo_url
+        targetRevision = var.argocd_app_repo_revision
+        path           = var.argocd_app_repo_path
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = var.argocd_app_destination_namespace
+      }
+      syncPolicy = var.argocd_app_sync_enabled ? {
+        automated = {
+          prune    = var.argocd_app_prune
+          selfHeal = var.argocd_app_self_heal
+        }
+      } : null
+    }
+  }
+
+  depends_on = [module.argocd]
+
+  timeouts {
+    create = "5m"
+  }
 }
 
 # Outputs to verify deployment values.
