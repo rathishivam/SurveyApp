2e3c45dd24f0fc6d56f9d5dd865dcfababec962f
rathishivam <shivamrathi011@gmail.com>
2026-05-25 21:27:35 +0530

    Add access configuration to EKS cluster for authentication mode and admin permissions

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -17,6 +17,10 @@ resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn
   version  = var.cluster_version
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
   vpc_config {
     subnet_ids = var.subnet_ids
 
