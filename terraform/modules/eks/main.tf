e829b055318d9675ccb6a13256631a8c95bde7ce
Shivam Rathi <64692938+rathishivam@users.noreply.github.com>
2026-05-25 13:28:48 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -17,7 +17,6 @@ resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn
   version  = var.cluster_version
-  enable_cluster_creator_admin_permissions = true
   vpc_config {
     subnet_ids = var.subnet_ids
 
